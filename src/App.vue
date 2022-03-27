e1a90055b660eb617d510bb8c63d67af729883bd
Alex-Rockwell <vin-ag@list.ru>
2022-03-27 20:55:33 +0400

    start

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="app" :class="mode">
+    <Header :mode="mode"/>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+  export default {
+  components: { Header },
+    data() {
+      return {
+        mode: 'light'
+      }
+    },
+  }
+</script>
+
+<style scoped>
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Roboto', sans-serif;
+  }
+  .app {
+    width: 100vw;
+    min-height: 100vh;
+    background-color: #f3f3f3;
+  }
+  .dark {
+    background-color: #15202B;
+    color: #f3f3f3;
+  }
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <header>
+      <h1>{{mode}} Mode</h1>
+      
+      <nav>
+        <div class="nav-link">Home</div>
+        <div class="nav-link">About</div>
+        <div class="nav-link">Info</div>
+        <div class="nav-link">Contact</div>
+      </nav>
+    </header>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      mode: {
+        type: String,
+        default: 'light'
+      },
+    },
+  }
+</script>
+
+<style scoped>
+  header {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 25px;
+    height: 60px;
+    background-color: #c3c3c3;
+    color: rgb(116, 116, 116);
+    transition:  background-color .3s ease-in;
+  }
+  header h1 {
+    font-size: 32px;
+    text-transform: uppercase;
+    font-weight: 900;
+  }
+  nav {
+    display: flex;
+    align-items: center;
+    flex-direction: row;
+  }
+  .nav-link {
+    display: flex;
+    align-items: center;
+    height: 60px;
+    padding: 0 15px;
+  }
+  .nav-link:hover {
+    cursor: pointer;
+    background-color: #b0b0b0;
+  }
+</style>
